bdc0c3196946946b735e239ba143f3d42102cae3
Kartikay Bhutani <kbhutani0001@gmail.com>
2018-10-07 20:34:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def home():
 			img=imgkit.from_file('tt.html', 'static/timetable.jpg')
 		
 		except Exception as e:
-			return ("Some error occurred, \n "+e)
+			return (("Some error occurred, \n {}").format(e))
 			
 			
 	return render_template("thanks.html",tt_dict=tt_dict)
